126cc07e4ed3327408e9d6814e1d87d5d0029a49
ByunghwanKim <34149981+ByunghwanKim@users.noreply.github.com>
2026-08-14 17:42:14 +0900

    Update Gbc_Db.py

--- Gbc_Db.py
+++ Gbc_Db.py
@@ -22,8 +22,8 @@ except KeyError:
 # 💡 구글 제미나이 AI 초기 설정
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel(
-    model_name='gemini-1.5-flash-latest',
-    generation_config={"response_mime_type": "application/json", "temperature": 0.2}
+    model_name='gemini-pro',
+    generation_config={"temperature": 0.2}
 )
 
 # 커스텀 CSS
